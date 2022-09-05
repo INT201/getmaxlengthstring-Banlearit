83c2f4dab0f56080ed15619f284ff4e9e8eef1b6
Banlearit Siriboon <banlearit.ares@mail.kmutt.ac.th>
2022-09-05 15:08:25 +0700

    code

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -2,5 +2,11 @@ const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
   //code here
+  if (arrayOfString === null || undefined) {
+    return undefined;
+  }else if (arrayOfString[arrayOfString.length-1]){
+    return arrayOfString;
+  }
+
 }
 module.exports = getMaxLengthString
