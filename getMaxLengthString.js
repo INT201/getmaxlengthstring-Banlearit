d392883f31c175506cc9fecffe8feed5ed6cc499
Banlearit Siriboon <banlearit.ares@mail.kmutt.ac.th>
2022-09-08 15:38:26 +0700

    code : update

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,24 +1,17 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  if (arrayOfString === null || arrayOfString === undefined) return undefined;
-  let max_str = arrayOfString[0];
-  for (const maxi of arrayOfString) {
-    if (maxi.length >= max_str[0].length) {
-      if (maxi.length === max_str[0].length) {
-        max_str.push(maxi)
-      } else {
-        max_str = [maxi];
-      }
+    if (arrayOfString === null || arrayOfString === undefined) return undefined;
+    let max_str = arrayOfString[0];
+    for (const maxi of arrayOfString) {
+        if (maxi.length >= max_str[0].length) {
+            if (maxi.length === max_str[0].length) {
+                max_str.push(maxi)
+            } else {
+                max_str = [maxi];
+            }
+        }
     }
-  }
-  return max_str;
+    return max_str;
 }
 module.exports = getMaxLengthString
-
-
-
-
-
-
-
